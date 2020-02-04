ff3285ce6c4501d56bb272de4a06046b25d45354
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-02-04 10:36:49 -1000

    part 5 Halfway complete

--- SwiftPlayground2.playground/Contents.swift
+++ SwiftPlayground2.playground/Contents.swift
@@ -26,3 +26,50 @@ else
     print("I have no idea what I'm doing")
     
 }
+
+//problem set 1
+
+var Keoki = 1
+
+var Nex = 5
+
+if Keoki > Nex
+{
+    print("As expected")
+}
+else
+{
+print ("It must be PubG or OW")
+
+}
+
+//problem set 2
+
+var Joshua = 10
+
+var Danny = 10
+
+if Joshua == Danny
+{
+ print ("Friends")
+}
+else
+{
+    print ("Still friends") }
+
+//problem set 3
+
+var Thomas = true
+
+var Bertie = false
+
+let train = Thomas
+
+let car = Bertie
+
+if train
+{
+    print("Hello Thomas!")}
+else
+{
+    print("That's not Thomas!")}
