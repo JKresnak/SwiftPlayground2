afeaf3a4d7d0c162a5909ae8985cb9b57281f2b9
Jkresnak <cmstudent@UHWO-ACM-01.uhwo.hawaii.edu>
2020-02-04 10:17:00 -1000

    SwiftPlayground2
    2-4-2020 Just completed Parts 1-4

---
+++ SwiftPlayground2.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Still attempting to push file to repository
+
+var goodGrade = "Completed work"
+
+if goodGrade == "Completed work"
+{
+print("I should be getting about a B")
+}
+
+else
+{
+    print("Work Harder")
+}
+
+var gradeA = true
+if gradeA
+    
+{ print("ez") }
+
+else
+{
+    print("I have no idea what I'm doing")
+    
+}
